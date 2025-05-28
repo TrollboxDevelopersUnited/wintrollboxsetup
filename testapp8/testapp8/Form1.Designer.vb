74f1b3c75736cbaeb51b10f74e3eb6fe6b6a1074
Miweoro <miweoro@outlook.pt>
2025-05-28 19:35:12 +0100

    Add files via upload

---
+++ testapp8/testapp8/Form1.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Label1 = New Label()
+        CheckBox1 = New CheckBox()
+        Label2 = New Label()
+        Label3 = New Label()
+        LinkLabel1 = New LinkLabel()
+        LinkLabel2 = New LinkLabel()
+        Button1 = New Button()
+        Label4 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(31, 30)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(572, 62)
+        Label1.TabIndex = 0
+        Label1.Text = "Trollbox Native App Setup"
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(485, 186)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(172, 24)
+        CheckBox1.TabIndex = 1
+        CheckBox1.Text = "what i am doing here"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(31, 110)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(646, 20)
+        Label2.TabIndex = 2
+        Label2.Text = "This is the TNAS (Trollbox Native App Setup) this setup will install the TNA (Trollbox Native App)"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(35, 145)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(223, 20)
+        Label3.TabIndex = 3
+        Label3.Text = "so just click the install button pls"
+        ' 
+        ' LinkLabel1
+        ' 
+        LinkLabel1.AutoSize = True
+        LinkLabel1.Location = New Point(38, 200)
+        LinkLabel1.Name = "LinkLabel1"
+        LinkLabel1.Size = New Size(160, 20)
+        LinkLabel1.TabIndex = 4
+        LinkLabel1.TabStop = True
+        LinkLabel1.Text = "github repo of the app"
+        ' 
+        ' LinkLabel2
+        ' 
+        LinkLabel2.AutoSize = True
+        LinkLabel2.Location = New Point(43, 240)
+        LinkLabel2.Name = "LinkLabel2"
+        LinkLabel2.Size = New Size(170, 20)
+        LinkLabel2.TabIndex = 5
+        LinkLabel2.TabStop = True
+        LinkLabel2.Text = "github repo of the setup"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(619, 375)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 6
+        Button1.Text = "Install"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label4.Location = New Point(31, 30)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(606, 62)
+        Label4.TabIndex = 7
+        Label4.Text = "Installing... (DO NOT CLOSE)"
+        Label4.Visible = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label4)
+        Controls.Add(Button1)
+        Controls.Add(LinkLabel2)
+        Controls.Add(LinkLabel1)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(CheckBox1)
+        Controls.Add(Label1)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "Form1"
+        Text = "Setup"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents LinkLabel2 As LinkLabel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label4 As Label
+
+End Class
---
+++ testapp8/testapp8/Form1.vb
@@ -0,0 +1,184 @@
+﻿Option Strict Off
+Imports System.IO
+Imports System.Net
+Imports System.Net.Http
+Imports System.Windows.Forms
+Imports System.Threading.Tasks
+Imports IWshRuntimeLibrary
+Imports System.Data.Common
+Imports SevenZipExtractor
+Imports SevenZip
+Public Class Form1
+
+    Private Sub LinkL(sender As Object, e As EventArgs) Handles LinkLabel1.Click
+        Try
+            Dim psi As New ProcessStartInfo()
+            psi.FileName = "https://github.com/TrollboxDevelopersUnited/WinTrollbox"
+            psi.UseShellExecute = True ' This ensures the system uses the default browser
+            Process.Start(psi)
+        Catch ex As System.ComponentModel.Win32Exception
+            MessageBox.Show($"Error: {ex.Message}",
+"Error",
+            MessageBoxButtons.OK,
+            MessageBoxIcon.Error)
+        End Try
+    End Sub
+    Private Sub LinkG(sender As Object, e As EventArgs) Handles LinkLabel2.Click
+        Try
+            Dim psi As New ProcessStartInfo()
+            psi.FileName = "https://github.com/TrollboxDevelopersUnited/wintrollboxsetup"
+            psi.UseShellExecute = True ' This ensures the system uses the default browser
+            Process.Start(psi)
+        Catch ex As System.ComponentModel.Win32Exception
+            MessageBox.Show($"Error: {ex.Message}",
+"Error",
+            MessageBoxButtons.OK,
+            MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Async Sub install(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Label1.Visible = False
+        Label2.Visible = False
+        Label3.Visible = False
+        Label4.Visible = True
+        LinkLabel1.Visible = False
+        LinkLabel2.Visible = False
+        Button1.Visible = False
+        CheckBox1.Visible = False
+
+
+        Try
+            ' URL of the file to download
+            Dim fileUrl As String = "https://github.com/TrollboxDevelopersUnited/WinTrollbox/releases/download/v5.5/wowa7zfile.7z"
+
+            Dim iconUrl As String = "https://www.windows93.net/favicon.ico?v=2.4.9"
+
+            Dim IcosavePath As String = Path.Combine(Path.GetTempPath(), "Trollbox\") & "ico.ico"
+            ' Path to save the downloaded file
+            Dim savePath As String = Path.Combine(Path.GetTempPath(), "Trollbox\") & "wowa7zfile.7z"
+            SevenZip.SevenZipExtractor.SetLibraryPath("C:\Program Files\7-Zip\7z.dll")
+            Dim sevenZup As New ProcessStartInfo()
+            sevenZup.FileName = "C:\Program Files\7-Zip\7z.exe"
+            sevenZup.Arguments = $"x ""{Path.Combine(Path.GetTempPath, "Trollbox\") & "wowa7zfile.7z"}"" -o""{Path.Combine(Path.GetTempPath(), "Trollbox")}"" -y"
+            sevenZup.WindowStyle = ProcessWindowStyle.Hidden
+            sevenZup.UseShellExecute = False
+
+
+            Dim extractor As New SevenZip.SevenZipExtractor(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads\") & "wowa7zfile.7z")
+            ' Specify the path of the folder to be created
+            Dim folderPath As String = Path.Combine(Path.GetTempPath(), "Trollbox")
+            If IO.File.Exists(folderPath) Then
+                Dim result1 As DialogResult = MessageBox.Show("Do you want to update the app?",
+                                                              "Update Confirmation",
+MessageBoxButtons.YesNo,
+MessageBoxIcon.Question
+                )
+                If result1 = DialogResult.No Then
+                    Environment.Exit(0)
+
+                End If
+            End If
+            Try
+                ' Check if the folder already exists
+                If Not Directory.Exists(folderPath) Then
+                    ' Create the folder
+                    Directory.CreateDirectory(folderPath)
+                    Console.WriteLine("Folder created.")
+
+
+                End If
+            Catch ex As Exception
+                ' Handle any errors
+                MessageBox.Show(
+                    $"Error creating folder: {ex.Message}",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error)
+            End Try
+            ' Download the file
+            Try
+                Using client As New HttpClient()
+
+                    Dim bytes1 As Byte() = Await client.GetByteArrayAsync(fileUrl)
+                    Await System.IO.File.WriteAllBytesAsync(savePath, bytes1)
+
+
+
+                    Try
+                        Dim proc As Process = Process.Start(sevenZup)
+                        proc.WaitForExit()
+                    Catch ex As Exception
+                        MessageBox.Show(
+                            $"Error: {ex.Message}",
+                            "Error",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error)
+                    End Try
+                    Dim bytes2 As Byte() = Await client.GetByteArrayAsync(iconUrl)
+                    Await System.IO.File.WriteAllBytesAsync(IcosavePath, bytes2)
+                End Using
+            Catch ex As Exception
+                MessageBox.Show(
+                    $"Error downloading file: {ex.Message}",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error)
+            End Try
+
+
+            If Not IO.File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) & "Trollbox.lnk") Then
+                Try
+
+                    Dim shortcutPath As String = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Trollbox.lnk") ' Where the shortcut will be saved
+                    Dim targetPath As String = Path.Combine(Path.GetTempPath(), "Trollbox\") & "net9.0-windows\testapp9.exe" ' Path to the application or file
+                    Dim description As String = "Trollbox Native App"
+
+                    ' Create the shortcut
+                    Dim shell As New WshShell()
+                    Dim shortcut As IWshShortcut = CType(shell.CreateShortcut(shortcutPath), IWshShortcut)
+                    shortcut.TargetPath = targetPath
+                    shortcut.IconLocation = IcosavePath
+                    shortcut.Description = description
+                    shortcut.Save()
+
+                    Console.WriteLine("System shortcut created successfully!")
+                Catch ex As Exception
+                    MessageBox.Show($"Error creating shortcut: {ex.Message}",
+                                "Error",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error
+)
+                End Try
+            Else
+                MessageBox.Show(
+                        "Shortcut already exists.",
+                        "Warning",
+            MessageBoxButtons.OK,
+            MessageBoxIcon.Warning)
+            End If
+
+            Dim result As DialogResult = MessageBox.Show("Setup Installed! You can now close the setup.",
+"Installed",
+            MessageBoxButtons.OK,
+            MessageBoxIcon.Information
+            )
+            If result = DialogResult.OK Then
+                Environment.Exit(0)
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(
+                $"Error downloading file: {ex.Message}",
+                "Error",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error
+                )
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
